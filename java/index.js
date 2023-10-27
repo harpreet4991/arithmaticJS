0cb25ad5af8cc417ddf3561beda47e4c8d2bc8e9
harpreet4991 <harpreet4991@gmail.com>
2023-10-27 13:47:35 +0530

    prog of js for change the value of varaibles

--- java/index.js
+++ java/index.js
@@ -8,13 +8,23 @@
 // var z=x-y
 // alert("subtraction result is "+z)
 
-var a=4
-var b=5
-var c=a*b
-alert("multiplication is "+c)
+// var a=4
+// var b=5
+// var c=a*b
+// alert("multiplication is "+c)
 
 
-// var x=10
+// var x=10git 
 // var y=5
 // var z=x/y
 // console.log(z)
+
+
+// var different method
+
+var x=100 
+ x=50
+ var y=50
+ 
+ z=x+y
+ console.log(z)
