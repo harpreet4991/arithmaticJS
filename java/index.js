cb91e9dd09d97c2b72c1442e6e3a34268d4bc20e
harpreet4991 <harpreet4991@gmail.com>
2023-10-27 13:20:22 +0530

    prog for division with js

---
+++ java/index.js
@@ -0,0 +1,20 @@
+// var x=5
+// var y=8
+// var z=x+y
+// console.log(z)
+
+// var x=8
+// var y=5
+// var z=x-y
+// alert("subtraction result is "+z)
+
+// var a=4
+// var b=5
+// var c=a*b
+// alert("multiplication is "+c)
+
+
+var x=10
+var y=5
+var z=x/y
+console.log(z)
